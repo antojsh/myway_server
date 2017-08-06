b85df3ebabdb009564f2a7d5c1ecc6a64b40f002
Antonio Sierra <antoniojsh93@gmail.com>
2017-08-06 17:59:31 -0500

    Prueba Model

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ var config = {
       name: 'mibus-server'
     },
     port: process.env.PORT || 3000,
-    db: 'mongodb://localhost/myway'
+    db: 'mongodb://antojsh:antonio199308JSH@ds129183.mlab.com:29183/myway'
   },
 
   test: {
