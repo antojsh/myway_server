00d973cefd9ef92178cb277295a5bc7a0030c609
Antonio Sierra <antoniojsh93@gmail.com>
2017-08-06 17:25:21 -0500

    Prueba Model

--- app/models/routes.js
+++ app/models/routes.js
@@ -11,7 +11,7 @@ var FleetSchema = new Schema({
   name: String,
   photo: String,
   loc: {
-    type: "LineString", // [<longitude>, <latitude>]
+    type: [], // [<longitude>, <latitude>]
     index: "2dsphere" // create the geospatial index
   }
 });
