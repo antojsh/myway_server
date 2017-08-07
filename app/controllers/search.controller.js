d6a10aa57d62bc535e5979396cf14936205af6ef
Antonio Sierra <antoniojsh93@gmail.com>
2017-08-07 17:09:11 -0500

    Prueba Model

--- app/controllers/search.controller.js
+++ app/controllers/search.controller.js
@@ -23,7 +23,7 @@ function findNear(coors, limit, maxDistance, type) {
             type: "Point",
             coordinates: coors
           },
-          $maxDistance: 40
+          $maxDistance: 80
 
         }
       }
